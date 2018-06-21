2c322a3dfcc00c6d7900fe2155eb9ddbb1b9479f
Zhixuan Feng <zhixuan_feng@hotmail.com>
2018-06-20 20:13:36 -0500

    Fixed bug in checking if password=confirm password

--- Timoc/src/servlets/AccountRegisterServlet.java
+++ Timoc/src/servlets/AccountRegisterServlet.java
@@ -65,9 +65,11 @@ public class AccountRegisterServlet extends HttpServlet
 			request.setAttribute("PasswordProblem", passwordProblem + "<br>");
 			shouldRetry = true;
 		}
-		else if (password.equals(request.getParameter("password_re")))  // TODO: learn how to do this in front end
+		else if (!password.equals(request.getParameter("password_re")))  // TODO: learn how to do this in front end
 		{
-			request.setAttribute("PasswordProblem", "The two passwords entered do not match, please try again." + "<br>");
+			passwordProblem = "The two passwords entered do not match, please try again.";
+			request.setAttribute("PasswordProblem", passwordProblem + "<br>");
+			shouldRetry = true;
 		}
 		if (!isValidEmail(email))
 		{
