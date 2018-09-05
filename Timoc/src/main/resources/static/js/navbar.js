e809cabc45db179f5751951c1924f0e99fb69680
Zhixuan Feng <zhixuan_feng@hotmail.com>
2018-09-05 01:30:17 -0500

    Updated join page with navigation bar

--- Timoc/src/main/resources/static/js/navbar.js
+++ Timoc/src/main/resources/static/js/navbar.js
@@ -1,5 +1,20 @@
 "use strict";
 
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function onMenuClicked() {
+    toTop();
+    let nav = document.getElementById("topnav");
+    if (nav.className === "topnav navbar-fixed-top") {
+        nav.className += " menuClicked";
+    } else {
+        nav.className = "topnav navbar-fixed-top";
+    }
+}
+
 let myInfo;
 $(document).ready(function() {
     const token = $("meta[name='_csrf']").attr("content");
